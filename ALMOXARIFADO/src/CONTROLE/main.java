7caf3af522a7fdef0ee4c620cefe8a7c4d7a2d46
Esmoocky <33699730+Esmoocky@users.noreply.github.com>
2017-11-15 17:55:21 -0200

    Add files via upload

---
+++ ALMOXARIFADO/src/CONTROLE/main.java
@@ -0,0 +1,17 @@
+
+package CONTROLE;
+
+import VIEW.TELAINICIAL;
+
+/**
+ *
+ * @author gabri
+ */
+public class main {
+  
+    public static void main (String Args[]){
+        new TELAINICIAL().setVisible(true);
+        
+        
+    }   
+}
